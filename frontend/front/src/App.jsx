6a1d88ba2173c1899410cdde220d4fc55c140282
NIpYo7 <nipunsuranimala40@gmail.com>
2025-08-23 03:41:18 +0530

    sign in page

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,10 +1,10 @@
-import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <Login />
     </div>
   )
 }
