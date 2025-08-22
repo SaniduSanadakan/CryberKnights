198b5ba053f31298fded6a0cba7cfa95a1bcf548
NIpYo7 <nipunsuranimala40@gmail.com>
2025-08-23 04:40:28 +0530

    as

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,10 +1,12 @@
 import Login from './pages/Login'
 import Footer from './components/Footer'
+import Header from './components/header'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Login />
       <Footer />
     </div>
---
+++ frontend/front/src/components/header.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+      <header className="canteen-header">
+        <div className="header-left">
+          <h1 className="logo">🍽 Canteen MIS</h1>
+          <nav className="nav-links">
+            <Link to="/">Dashboard</Link>
+            <Link to="/orders">Orders</Link>
+            <Link to="/menu">Menu</Link>
+            <Link to="/tokens">Tokens</Link>
+            <Link to="/feedback">Feedback</Link>
+          </nav>
+        </div>
+        <div className="header-right">
+          <Link to="/signup" className="btn signup">Sign Up</Link>
+          <Link to="/login" className="btn login">Login</Link>
+        </div>
+      </header>
+    );
+  }
