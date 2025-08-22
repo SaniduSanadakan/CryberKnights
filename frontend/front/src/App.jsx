7a0b69589b006971c0e2136ab900e1c4ea5f5b99
NIpYo7 <nipunsuranimala40@gmail.com>
2025-08-23 05:25:38 +0530

    we

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,15 +1,34 @@
-import Login from './pages/Login'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Orders from './pages/Orders'
+import Menu from './pages/Menu'
+import Tokens from './pages/Tokens'
+import Feedback from './pages/Feedback'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Login />
-      <Footer />
-    </div>
+    
+      <div className="App">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/tokens" element={<Tokens />} />
+            <Route path="/feedback" element={<Feedback />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    
   )
 }
 
---
+++ frontend/front/src/pages/Feedback.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Feedback = () => {
+  return (
+    <div style={{ paddingTop: '100px', textAlign: 'center' }}>
+      <h1>Feedback Page</h1>
+      <p>Share your feedback about our services</p>
+    </div>
+  );
+};
+
+export default Feedback;
